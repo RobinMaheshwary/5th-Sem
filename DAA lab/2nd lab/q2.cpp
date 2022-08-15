76a923bb482686ba5d17da1360196ca12ca9d270
Robin <robinmaheshwari1234567@gmail.com>
2022-08-16 02:55:01 +0530

    q

--- DAA lab/2nd lab/q2.cpp
+++ DAA lab/2nd lab/q2.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int static dp[1001][1001];
@@ -56,39 +56,39 @@ int Consecutive_integer_checking(int a, int b)
 
 int gcd(int a, int b)
 {
-    
+
     // Everything divides 0
     if (a == 0)
         return b;
     if (b == 0)
         return a;
- 
+
     // base case
     if (a == b)
         return a;
-     
+
     // if a value is already
     // present in dp
-    if(dp[a][b] != -1)
+    if (dp[a][b] != -1)
         return dp[a][b];
- 
+
     // a is greater
     if (a > b)
-        dp[a][b] = gcd(a-b, b);
-     
+        dp[a][b] = gcd(a - b, b);
+
     // b is greater
     else
-        dp[a][b] = gcd(a, b-a);
-     
+        dp[a][b] = gcd(a, b - a);
+
     // return dp
     return dp[a][b];
 }
 
 int main()
 {
-    clock_t  start1,end1;
-    clock_t  start2,end2;
-    clock_t  start3,end3;
+    clock_t start1, end1;
+    clock_t start2, end2;
+    clock_t start3, end3;
     cout << "\n\nrobin,20051828\n\n";
     int number1, number2;
 
@@ -100,28 +100,28 @@ int main()
         return 0;
     }
     else
-    start1 = clock();
-    cout<<"gcd1= "<<Euclid(number1, number2);
-    end1 = clock();
-    double time_taken1 = double(end1 - start1) / double(CLOCKS_PER_SEC);
-    cout << "\n\nTime taken by program is :" << fixed << time_taken1 << setprecision(5);
-    cout << "sec" << endl;
+    {
+        start1 = clock();
+        cout << "gcd1= " << Euclid(number1, number2);
+        end1 = clock();
+        double time_taken1 = double(end1 - start1) / double(CLOCKS_PER_SEC);
+        cout << "\n\nTime taken by program is :" << fixed << time_taken1 << setprecision(5);
+        cout << "sec" << endl;
 
-    start2 = clock();
-    cout<<"gcd2= "<<Consecutive_integer_checking(number1, number2);
-    end2 = clock();
-    double time_taken2 = double(end2 - start2) / double(CLOCKS_PER_SEC);
-    cout << "\n\nTime taken by program is :" << fixed << time_taken2 << setprecision(5);
-    cout << "sec" << endl;
+        start2 = clock();
+        cout << "gcd2= " << Consecutive_integer_checking(number1, number2);
+        end2 = clock();
+        double time_taken2 = double(end2 - start2) / double(CLOCKS_PER_SEC);
+        cout << "\n\nTime taken by program is :" << fixed << time_taken2 << setprecision(5);
+        cout << "sec" << endl;
 
-    start3 = clock();
-    memset(dp, -1, sizeof(dp));
-    cout<<"gcd3= "<<gcd(number1, number2);
-    end3 = clock();
-    double time_taken3 = double(end3 - start3) / double(CLOCKS_PER_SEC);
-    cout << "\n\nTime taken by program is :" << fixed << time_taken3 << setprecision(5);
-    cout << "sec" << endl;
-    
-    
+        start3 = clock();
+        memset(dp, -1, sizeof(dp));
+        cout << "gcd3= " << gcd(number1, number2);
+        end3 = clock();
+        double time_taken3 = double(end3 - start3) / double(CLOCKS_PER_SEC);
+        cout << "\n\nTime taken by program is :" << fixed << time_taken3 << setprecision(5);
+        cout << "sec" << endl;
+    }
     return 0;
 }
